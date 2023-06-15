751fde482c2f4bab8fd887c089cf39256aad9ce6
Muriuki Muchiri <muriuki@muchir.com>
2023-06-15 10:17:38 +0000

    Correcting Advanced Task 2: using fwrite instead of perror function.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -8,6 +9,7 @@
 
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	, 59, 1, stderr);
 	return (1);
 }
