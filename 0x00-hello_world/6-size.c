fb5d0441d96fefb8ab116eab042da8bffedf8b4e
Muriuki Muchiri <muriuki@muchir.com>
2023-06-15 09:07:26 +0000

    Added Task 6.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d\n", (unsigned short)sizeof(char));
+	printf("Size of an int: %d\n", (unsigned short)sizeof(int));
+	printf("Size of a long int: %d\n", (unsigned short)sizeof(long int));
+	printf("Size of a long long int: %d\n", (unsigned short)sizeof(long long int));
+	printf("Size of a float: %d\n", (unsigned short)sizeof(float));
+	return (0);
+}
