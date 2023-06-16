32622a9a09ed27a9a42bb269d4b790fd2f9767b7
Muriuki Muchiri <muriuki@muchir.com>
2023-06-16 08:05:06 +0000

    Added program 7.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	for (int i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
