e48a027a6b47ab908d74a17e005867d28d071466
Muriuki Muchiri <muriuki@muchir.com>
2023-06-16 08:03:05 +0000

    Added program 6.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	for (int i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
