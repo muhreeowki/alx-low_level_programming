bcef2b194f0938cb905e5f2d306c60d82428c86c
Muriuki Muchiri <muriuki@muchir.com>
2023-06-16 07:31:41 +0000

    Added C programs for task 1 and 2.

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	int i = n % 10;
+
+	if (i == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, i);
+	}
+	else if (i > 5)
+	{
+		printf("Last digit of %d is %d and is and is greater than 5\n", n, i);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, i);
+	}
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
