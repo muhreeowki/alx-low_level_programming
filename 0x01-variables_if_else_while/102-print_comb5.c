5876a24554985a48dc47306ad6577fc3bbe0fa46
Muriuki Muchiri <muriuki@muchir.com>
2023-06-16 13:05:01 +0000

    Fixing styling errors.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,14 +14,14 @@ int main(void)
 
 	for (i = 0; i < 99; i++)
 	{
-		for (j = i + 1; j <= 99; j++) 
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar(floor(i / 10) + 48);
 			putchar(i % 10 + 48);
 			putchar(' ');
 			putchar(floor(j / 10) + 48);
 			putchar(j % 10 + 48);
-			if(i >= 98 && j >= 99)
+			if (i >= 98 && j >= 99)
 				continue;
 			putchar(',');
 			putchar(' ');
