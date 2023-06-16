7fd4baf0f4a6c06985420108d4695245520be351
Muriuki Muchiri <muriuki@muchir.com>
2023-06-16 07:34:00 +0000

    Added program 3.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (int i = 65; i <= 90; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
