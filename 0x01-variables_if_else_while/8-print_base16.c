c3e56ab41c532b0d279f90b6426caa911419170f
Muriuki Muchiri <muriuki@muchir.com>
2023-06-16 08:07:24 +0000

    Adding program 8.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	for (int i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (int i = 97; i <= 102; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
