1745212ffcd4105b62c902c608f050f908ae31e0
Muriuki Muchiri <muriuki@muchir.com>
2023-06-20 10:02:40 +0000

    Fixing format error in program 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,19 +13,19 @@ int print_sign(int n)
 {
 	if (n == 0)
 	{
-		putchar('0');
+		putchar(48);
 		return (0);
 	}
 
 	else if (n > 0)
 	{
-		putchar('+');
+		putchar(43);
 		return (1);
 	}
 
 	else
 	{
-		putchar('-');
+		putchar(45);
 		return (-1);
 	}
 }
