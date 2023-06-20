d403b11ef90a0efa3aaa3edea6c20aa3f1804df0
Muriuki Muchiri <muriuki@muchir.com>
2023-06-20 08:17:08 +0000

    Correcting program 9: Fixed spacing

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <math.h>
+
+/* Function prototype */
+void times_table(void);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
+
+
+/**
+ * times_table - Prints the times table for base 10 numbers
+ */
+void times_table(void)
+{
+	int x;
+	int y;
+	int product;
+
+	y = 0;
+	while (y < 10)
+	{
+		x = 0;
+		while (x < 10)
+		{
+			product = y * x;
+			if (product >= 10)
+			{
+				putchar(floor(product / 10) + '0');
+				putchar(product % 10 + '0');
+			}
+			else
+				putchar(product + '0');
+
+			if(x != 9)
+			{
+				putchar(',');
+				putchar(' ');
+				if ((x + 1) * y < 10)
+					putchar(' ');
+			}
+			x++;
+		}
+		putchar('\n');
+		y++;
+	}
+
+}
