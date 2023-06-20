902d9a10b88e5887a9e70f88117b0fddb1da8520
Muriuki Muchiri <muriuki@muchir.com>
2023-06-20 06:41:48 +0000

    Added program 4.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/* Function prototype */
+int _isalpha(int c);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	return (0);
+}
+
+
+/**
+ * _isalpha - Checks if a char is alphabetical or not
+ *
+ * @c: ASCII value of the char
+ *
+ * Return: 0 if alpha, 1 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c < 65 || c > 122 || (c > 90 && c < 97)
+	{
+		return (1);
+	}
+	return (0);
+}
+
