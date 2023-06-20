939799d973052589d72ecd465578b9a0d1317376
Muriuki Muchiri <muriuki@muchir.com>
2023-06-20 06:23:28 +0000

    Added program 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/* Function prototype */
+int _islower(int c);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	return (0);
+}
+
+
+/**
+ * _islower - Checks if a char is lowercase or not
+ *
+ * @c: ASCII value of the char
+ *
+ * Return: 0 if lowercase, 1 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c < 97 || c > 122)
+	{
+		return (1);
+	}
+	return (0);
+}
+
