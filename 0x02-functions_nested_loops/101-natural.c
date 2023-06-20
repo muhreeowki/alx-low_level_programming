9deaf56e42f74ead35c16e14a67d3856af331014
Muriuki Muchiri <muriuki@muchir.com>
2023-06-20 14:38:26 +0000

    Added program 102

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int sum;
+	int n;
+
+	sum = 0;
+	n = 0;
+	while(n < 1024)
+	{
+		if (n % 3 == 0 || n % 5 == 0) 
+		{
+			sum += n;
+		}
+		n++;
+	}
+	print_number(sum);
+	putchar('\n');
+}
+
+/**
+ * print_number - Helper function to recursivly print the individual
+ * digits of an integer n using only the putchar function.
+ *
+ * @n: integer to be printed
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	putchar(n % 10 + '0');
+}
