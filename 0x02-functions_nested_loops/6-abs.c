7f6aae3aad90ff3b38b071c97b2e230cab419e8b
Muriuki Muchiri <muriuki@muchir.com>
2023-06-20 07:04:04 +0000

    Added program 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/* Function prototype */
+int _abs(int);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	return (0);
+}
+
+
+/**
+ * _abs - computes the absolute value of a number.
+ *
+ * @n: any number
+ *
+ * Return: the absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
+
+
