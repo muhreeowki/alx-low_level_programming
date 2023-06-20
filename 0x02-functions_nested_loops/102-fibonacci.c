e9ce9f891412b579388e3ff0ee41ebbb060d4602
Muriuki Muchiri <muriuki@muchir.com>
2023-06-20 16:29:28 +0000

    Using c90 data types

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
+void print_long_number(unsigned long int n);
+
 /**
  * main - Entry point
  *
  * Return: Always 0
  */
-
-void print_long_number(unsigned long long int n);
-
 int main(void)
 {
 	fibonacci(50);
@@ -17,23 +16,29 @@ int main(void)
 }
 
 
+
+/**
+ * fibonacci - Prints the first n values of the fibonacci sequence
+ *
+ * @n: Number of fibonacci numbers to print
+ */
 void fibonacci(int n)
 {
 	unsigned short int i;
-	unsigned long long int a;
-	unsigned long long int b;
-	unsigned long long int fib;
+	unsigned long int a;
+	unsigned long int b;
+	unsigned long int fib;
 
 	a = 1;
 	b = 1;
 
-	for(i = 2; i <= n + 1; i++ )
+	for (i = 2; i <= n + 1; i++)
 	{
 		print_long_number(a);
 		fib = a + b;
 		a = b;
 		b = fib;
-		if(i != n + 1)
+		if (i != n + 1)
 		{
 			putchar(',');
 			putchar(' ');
@@ -43,20 +48,14 @@ void fibonacci(int n)
 
 
 /**
- * print_number - Helper function to recursivly print the individual
- * digits of an integer n using only the putchar function.
+ * print_long_number - Helper function to recursivly print the individual
+ * digits of a long long integer n using only the putchar function.
  *
  * @n: integer to be printed
  */
 
-void print_long_number(unsigned long long int n)
+void print_long_number(unsigned long int n)
 {
-	if (n < 0)
-	{
-		putchar('-');
-		n = -n;
-	}
-
 	if (n / 10)
 		print_long_number(n / 10);
 
