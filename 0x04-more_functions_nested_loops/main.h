86a183cd0e8b842d8d730d2eca32a8532f954f81
Muriuki Muchiri <muriuki@muchir.com>
2023-06-22 11:22:20 +0000

    Using _putchar instead of putchar

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,10 +5,10 @@
 
 int _isupper(int c);
 int _isdigit(int c);
+int _putchar(char c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-int _putchar(char c);
 void more_numbers(void);
 
 #endif
