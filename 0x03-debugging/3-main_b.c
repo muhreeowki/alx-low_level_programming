1c370619cdbb79c48cbb452fcedcb79438f8f57a
Muriuki Muchiri <muriuki@muchir.com>
2023-06-22 07:26:37 +0000

    Updating conditional statements

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -14,8 +14,8 @@ int main(void)
 	int year;
     
 	month = 2;
-	day = 2;
-	year = 2000;
+	day = 29;
+	year = 2003;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 	day = convert_day(month, day);
