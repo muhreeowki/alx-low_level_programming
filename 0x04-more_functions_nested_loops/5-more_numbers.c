31731d5547dd373ef20b5e8bd549eea068021747
Muriuki Muchiri <muriuki@muchir.com>
2023-06-22 11:20:18 +0000

    Fixing logical errors in 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,18 +9,15 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for(j = 0; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j >= 10 )
+			if (j >= 10 && j / 10 != 0)
 			{
-				if (j / 10 != 0)	
-				{
-					putchar(j / 10 + '0');
-				}
+				putchar(j / 10 + '0');
 			}
-			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
 		}
 	}
 	_putchar('\n');
