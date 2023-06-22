d786d6545e026b3f589b9b8982010bb0cb72a99d
Muriuki Muchiri <muriuki@muchir.com>
2023-06-22 09:11:49 +0000

    Added function mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - mulitpilies two numbers a and b and returns the product
+ *
+ * @a: Number 1
+ * @b: Number 2
+ *
+ * Return: product of a and b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
