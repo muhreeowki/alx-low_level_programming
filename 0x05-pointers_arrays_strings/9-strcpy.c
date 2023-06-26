73cb639d81143aacf063dc34b06a9383eba5ada3
Muriuki Muchiri <muriuki@muchir.com>
2023-06-26 13:30:33 +0000

    Added file 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies 'src' string to a 'dest' buffer
+ *
+ * @src: Pointer to string to be copied
+ * @dest: Pointer to destination of copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int length;
+	int i;
+
+	length = _strlen(src) + 1;
+
+	for (i = 0; i < length; i++)
+	{
+		*(dest + i) = src[i];
+	}
+
+	return (dest);	
+}
+
+
+/**
+ * _strlen - Returns the length of a string
+ *
+ * @s: Address of the first char in the string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	char c;
+	int i;
+
+	i = 0;
+	c = *s;
+
+	while (c != '\0')
+	{
+		i++;
+		c = *(s + i);
+	}
+
+	return (i);
+}
