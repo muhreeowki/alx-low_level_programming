9ee9e85ae5ceab0e3558424845766de33af5d777
Muriuki Muchiri <muriuki@muchir.com>
2023-06-26 08:38:48 +0000

    Added file 2

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int a;
-	int b;
+    char *str;
+    int len;
 
-	a = 98;
-	b = 42;
-
-	printf("a=%d, b=%d\n", a, b);
-	swap_int(&a, &b);
-	printf("a=%d, b=%d\n", a, b);
-	return (0);
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
+    return (0);
 }
