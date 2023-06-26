aff87ec64259a8420f03b4631758397e2cba5cac
Muriuki Muchiri <muriuki@muchir.com>
2023-06-26 13:19:01 +0000

    Fixing styling erros

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,14 @@
 void print_array(int *a, int n)
 {
 	int i;
+
 	i = 0;
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", *(a + i));
+		printf("%d", *(a + i));
+		if (i != n - 1)
+			printf(", ");
 	}
 	printf("\n");
 }
