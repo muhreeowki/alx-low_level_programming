83a62bfc979982f7ddac92be9794f5fb4214059e
Muriuki Muchiri <muriuki@muchir.com>
2023-06-29 10:54:08 +0000

    :

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+/**
+ * _strcmp - copmares two strings case sensitivly.
+ *
+ * @s1: pointer to string to be compared
+ * @s2: pointer to string to compare to
+ *
+ * Return: 0 if strings are equal
+ * 1 if the first non-matching character in str1 is greater than that of str2
+ * -1 if the first non-matching character in str1 is less than that of str2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+
+	while (*(s1 + i) != '\0')
+	{
+		if (*(s1 + i) > *(s2 + i))
+			return (1);
+		if (*(s1 + i) < *(s2 + i))
+			return (-1);
+		i++;
+	}
+
+	return (0);
+}
