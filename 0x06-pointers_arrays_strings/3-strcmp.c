e0b01e2f0c7acc1925a180bdf3bcec265c5ceea0
Muriuki Muchiri <muriuki@muchir.com>
2023-06-29 15:22:19 +0000

    Handling edge cases in _strcmp function.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -18,7 +18,7 @@ int _strcmp(char *s1, char *s2)
 
 	i = 0;
 
-	while (*(s1 + i) != '\0' || *(s2 + i) != '\0')
+	while (*(s1 + i) != '\0' && *(s2 + i) != '\0')
 	{
 		if (*(s1 + i) > *(s2 + i))
 			return (15);
@@ -27,5 +27,16 @@ int _strcmp(char *s1, char *s2)
 		i++;
 	}
 
-	return (0);
+	if (*(s1 + i) == '\0' && *(s2 + i) != '\0')
+	{
+		return (15);
+	}
+	else if (*(s1 + i) != '\0' && *(s2 + i) == '\0')
+	{
+		return (-15);
+	}
+	else
+	{
+		return (0);
+	}
 }
