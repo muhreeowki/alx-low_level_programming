e237d98c0a1f7096bdb016849f92ce92d1ae6109
Muriuki Muchiri <muriuki@muchir.com>
2023-06-29 10:08:34 +0000

    Added file 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * _strncpy - Copies a string
+ *
+ * @src: pointer to string that is being coppied
+ * @dest: pointer to destination of the coppied string
+ * @n: number of bytes to copy from src
+ *
+ * Return: Pointer to the coppied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (*(src + i) != '\0' && i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	*(dest + i) = '\0';
+
+	return (dest);
+}
