4ce3f6d19c9f5648baac2df2a3f37ba82fff8e33
Muriuki Muchiri <muriuki@muchir.com>
2023-06-29 11:26:09 +0000

    Adjusting conditionals

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -18,7 +18,7 @@ int _strcmp(char *s1, char *s2)
 
 	i = 0;
 
-	while (*(s1 + i) != '\0' && *(s2 + i) != '\0')
+	while (*(s1 + i) != '\0' || *(s2 + i) != '\0')
 	{
 		if (*(s1 + i) > *(s2 + i))
 			return (15);
@@ -26,12 +26,6 @@ int _strcmp(char *s1, char *s2)
 			return (-15);
 		i++;
 	}
-
-	if (*(s1 + i) > *(s2 + i))
-		return (15);
-
-	if (*(s1 + i) < *(s2 + i))
-		return (-15);
 
 	return (0);
 }
