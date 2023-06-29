d4e07b647812fa1533e318f6bfd13b56abf4f781
Muriuki Muchiri <muriuki@muchir.com>
2023-06-29 15:43:45 +0000

    Reconstructing the loop to handle edge cases

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -18,25 +18,16 @@ int _strcmp(char *s1, char *s2)
 
 	i = 0;
 
-	while (*(s1 + i) != '\0' && *(s2 + i) != '\0')
+	while (0 < 2)
 	{
 		if (*(s1 + i) > *(s2 + i))
 			return (15);
 		if (*(s1 + i) < *(s2 + i))
 			return (-15);
+		if (*(s1 + i) == '\0' && *(s2 + i) == '\0')
+			return (0);
 		i++;
 	}
 
-	if (*(s1 + i) == '\0' && *(s2 + i) != '\0')
-	{
-		return (-15);
-	}
-	else if (*(s1 + i) != '\0' && *(s2 + i) == '\0')
-	{
-		return (15);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
--- 0x06-pointers_arrays_strings/zain.c
+++ 0x06-pointers_arrays_strings/zain.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-    char s1[] = "Hello";
+    char s1[] = "Hellodfghfd";
     char s2[] = "World!";
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
+    printf("%d\n", strcmp(s1, s2));
+    printf("%d\n", strcmp(s2, s1));
+    printf("%d\n", strcmp(s1, s1));
     return (0);
 }
