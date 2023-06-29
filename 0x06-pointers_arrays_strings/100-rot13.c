ac3c901e8adeea0f82d6e544fa89990861c81596
Muriuki Muchiri <muriuki@muchir.com>
2023-06-29 17:55:29 +0000

    Added file 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string into rot13 format
+ *
+ * @s: Pointer to the string
+ *
+ * Return: Pointer to the string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == alphabet[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
