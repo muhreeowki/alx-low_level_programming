67ff801c1f82abe1a9d586af007b653fd6da9f90
Muriuki Muchiri <muriuki@muchir.com>
2023-06-29 15:28:06 +0000

    Handling edge cases

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -29,11 +29,11 @@ int _strcmp(char *s1, char *s2)
 
 	if (*(s1 + i) == '\0' && *(s2 + i) != '\0')
 	{
-		return (15);
+		return (-15);
 	}
 	else if (*(s1 + i) != '\0' && *(s2 + i) == '\0')
 	{
-		return (-15);
+		return (15);
 	}
 	else
 	{
