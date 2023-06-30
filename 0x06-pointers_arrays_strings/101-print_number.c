c63be4a8a51cbd1ee6013562c80f161ea0f3a231
Muriuki Muchiri <muriuki@muchir.com>
2023-06-30 12:31:51 +0000

    Using unsigned int to handle large numbers in print_number function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,17 +9,20 @@
 
 void print_number(int n)
 {
+	unsigned int number;
 	/* Handle negative numbers */
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
-	}
+	} 
 
-	if (n / 10 > 0)
+	number = n;
+
+	if (number / 10 > 0)
 	{
-		print_number(n / 10);
+		print_number(number / 10);
 	}
 
-	_putchar((n % 10) + 48);
+	_putchar((number % 10) + 48);
 }
--- 0x06-pointers_arrays_strings/zain.c
+++ 0x06-pointers_arrays_strings/zain.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	print_number(-2147483647);
+	print_number(10);
 	_putchar('\n');
 	return (0);
 }
