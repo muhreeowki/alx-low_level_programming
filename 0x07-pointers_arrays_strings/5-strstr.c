41fdc7ed14789f43be9a375054ce6364e8024a31
Muriuki Muchiri <muriuki@muchir.com>
2023-07-03 19:56:01 +0000

    Rewrote file 5 using while loop instead of for loop

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,21 +15,18 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	int i, j;
+
+
 	for (i = 0; *(haystack + i) != '\0'; i++)
 	{
-		if (*(haystack + i) == *(needle))
-		{
-			for (j = 0; *(needle + j) != '\0'; j++)
-			{
-				if (*(haystack + (i + j)) != *(needle + j) || *(haystack + (i + j)) == '\0')
-					break;
-			}
+		j = 0;
 
+		while (*(haystack + (i + j)) == *(needle + j) && *(needle + j) != '\0')
 			j++;
 
-			if (*(needle + j) == '\0')
-				return ((haystack + i));
-		}
+		if (*(needle + j) == '\0')
+			return ((haystack + i));
 	}
 
 	return (NULL);
