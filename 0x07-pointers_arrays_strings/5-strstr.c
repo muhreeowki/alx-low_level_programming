8984a61ffb2c1d6a826c903077cb36f6144d969c
Muriuki Muchiri <muriuki@muchir.com>
2023-07-03 19:44:09 +0000

    Using pointer arithmetic instead of subscripts

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,18 +19,18 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 	int j;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (i = 0; *(haystack + i) != '\0'; i++)
 	{
-		if (haystack[i] == needle[0])
+		if (*(haystack + i) == needle[0])
 		{
 			for (j = 0; needle[j] != '\0'; j++)
 			{
-				if (haystack[i + j] != needle[j] || haystack[i + j] == '\0')
+				if (*(haystack + i + j) != *(needle + j) || *(haystack + i + j) == '\0')
 					break;
 			}
 
-			if (needle[j] == '\0')
-				return (haystack + i);
+			if (*(needle + j) == '\0')
+				return (*(haystack + i));
 		}
 	}
 
