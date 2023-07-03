24bdc5f1721930afe1fd198b048c5adb3a87409e
Muriuki Muchiri <muriuki@muchir.com>
2023-07-03 19:41:25 +0000

    Editing return conditions for file 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -26,14 +26,11 @@ char *_strstr(char *haystack, char *needle)
 			for (j = 0; needle[j] != '\0'; j++)
 			{
 				if (haystack[i + j] != needle[j] || haystack[i + j] == '\0')
-				{
-					j = 0;
 					break;
-				}
 			}
 
-			if (j > 0)
-				return (haystack);
+			if (needle[j] == '\0')
+				return (haystack + i);
 		}
 	}
 
