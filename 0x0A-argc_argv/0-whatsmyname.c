a79ce508d72dc68224e75a194af3e8680149bc7d
Muriuki Muchiri <muriuki@muchir.com>
2023-07-07 08:04:29 +0000

    Created 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints out its name
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+		printf("%s\n", argv[0]);
+	return (0);
+}
