b503317f982831f6d5993a0a2c60283b9a9a410e
Muriuki Muchiri <muriuki@muchir.com>
2023-07-07 09:22:18 +0000

    Created 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - This program prints out its name
+ *
+ * @argc: number of arguments passed into the program
+ * @argv: array of arguments passed into the program
+ *
+ * Return: Always 0 (success) 1 (error)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		j = atoi(argv[i]);
+
+		if (j == 0 && *(argv[i]) != '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += j;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
