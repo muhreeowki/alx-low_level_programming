9e701153b4c815e4667d18a6db27828d414b0bd0
Muriuki Muchiri <muriuki@muchir.com>
2023-07-10 09:03:10 +0000

    Created 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * str_concat - concatenates two strings
+ *
+ * @s1: pointer to string
+ * @s2: pointer to string
+ *
+ * Return: Pointer to string, or NULL if it fails.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, size;
+	char *new_string;
+
+	/* Find the size of s1 and s2 */
+
+	i = 0;
+	j = 0;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	size = (i + j) - 1;
+
+	new_string = (char *) malloc(sizeof(char) * size);
+
+	if (new_string == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		new_string[i] = s1[i];
+
+	for (j = 0; s2[j] != '\0'; j++)
+		new_string[i + j] = s2[j];
+
+	new_string[i + j] = '\0';
+
+	return (new_string);
+}
