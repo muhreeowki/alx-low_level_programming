1ed8a7afa2f79a28738c988a2a5750a8235ff50c
Muriuki Muchiri <muriuki@muchir.com>
2023-07-10 14:06:59 +0000

    Freeing memory when program fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 		temp = calloc(width, sizeof(int));
 		if (temp == NULL)
 		{
-			free(temp);
+			free(array);
 			for (j = 0; j < i; j++)
 				free(*(array + j));
 			return (NULL);
