922e03fb8ba9c817759c6500a04d63f4d853e566
Muriuki Muchiri <muriuki@muchir.com>
2023-07-10 10:08:43 +0000

    Handling NULL edge cases

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,6 +3,25 @@
 #include <stddef.h>
 
 /**
+ * _strlen - finds the length of a given string
+ *
+ * @str: pointer to string
+ *
+ * Return: Length of string, NULL if pointer is NULL
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+
+
+/**
  * str_concat - concatenates two strings
  *
  * @s1: pointer to string
@@ -28,16 +47,9 @@ char *str_concat(char *s1, char *s2)
 		return (new_string);
 	}
 
-
 	/* Find the size of s1 and s2 */
-	i = 0;
-	j = 0;
-
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-
-	for (j = 0; s2[j] != '\0'; j++)
-		;
+	i = _strlen(s1);
+	j = _strlen(s2);
 
 	size = i + j + 1;
 
@@ -45,7 +57,6 @@ char *str_concat(char *s1, char *s2)
 
 	if (new_string == NULL)
 		return (NULL);
-
 
 	/* Concatenate */
 	for (i = 0; s1[i] != '\0'; i++)
