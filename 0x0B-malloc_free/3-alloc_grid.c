6e3eed31be6cbb50df4ca0322ef52190f1e8c800
Muriuki Muchiri <muriuki@muchir.com>
2023-07-10 14:09:27 +0000

    free memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
 		if (temp == NULL)
 		{
 			free(array);
-			for (j = 0; j < i; j++)
+			for (j = 0; j <= i; j++)
 				free(*(array + j));
 			return (NULL);
 		}
