84b56d85b9799b8787e60b40559ce1ba42ea4f20
Muriuki Muchiri <muriuki@muchir.com>
2023-07-13 09:10:34 +0000

    Created 1-string_nconcat.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 
 /**
- *  malloc_checked - allocates memory using malloc
+ * malloc_checked - allocates memory using malloc
  *
+ * @b: Size of memory to allocate (in bytes)
  *
- *
+ * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
+
 	ptr = malloc(b);
 
 	if (ptr == NULL)
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - finds the length of a string
+ *
+ * @str: pointer to string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	return (i);
+}
+
+
+/**
+ * string_nconcat - concatenates n chars from s2 to s1
+ *
+ * @s1: String 1
+ * @s2: String 2
+ * @n: number of chars to concatenate
+ *
+ * Return: a pointer to the concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *newstr;
+	unsigned int i, j;
+
+	if (s2 == NULL)
+		*s2 = '\0';
+	if (s1 == NULL)
+		*s1 = '\0';
+
+	newstr = malloc(_strlen(s1) + _strlen(s2) + 1);
+
+	if (newstr == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		newstr[i] = s1[i];
+
+	for (j = 0; s2[j] != '\0' && j < n; j++)
+		newstr[i + j] = s2[j];
+
+	newstr[i + j] = '\0';
+
+	return (newstr);
+}
