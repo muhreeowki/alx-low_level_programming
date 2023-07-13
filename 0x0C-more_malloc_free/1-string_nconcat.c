404a46e2022de4f640099f7613e8eba58a0502ea
Muriuki Muchiri <muriuki@muchir.com>
2023-07-13 21:20:28 +0000

    Handling edge cases

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,11 +31,12 @@ int _strlen(char *str)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *newstr;
-	unsigned int i, j, len1, len2;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	i = j = 0;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
 	if (n >= len2)
 		newstr = malloc(sizeof(char) * (len1 + len2 + 1));
