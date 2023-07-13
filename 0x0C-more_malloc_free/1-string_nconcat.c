5916d827abe7e8c356d943e9ca729af27b840938
Muriuki Muchiri <muriuki@muchir.com>
2023-07-13 21:24:23 +0000

    Cleaning up code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,9 @@ int _strlen(char *str)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *newstr;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int i, j, len1, len2;
+
+	len1 = len2 = i = j = 0;
 
 	while (s1 && s1[len1])
 		len1++;
