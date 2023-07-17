e923f954e65e6b41baf5d3c83ae0bca1d0d9bb08
Muriuki Muchiri <muriuki@muchir.com>
2023-07-17 19:27:43 +0000

    Fixing format of printing

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,8 +12,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
-		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 	}
-
 }
