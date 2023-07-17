e724441088dd44da35d788d8ed91ecd34c2144fa
Muriuki Muchiri <muriuki@muchir.com>
2023-07-17 21:31:01 +0000

    Fixing styling errors

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,13 +6,9 @@ int _strlen(char *str);
 char *_strcpy(char *str, char *dest);
 
 /**
- * new_dog - creates a new dog
+ * free_dog - frees dog struct from memory
  *
- * @name: name to assign to the dog
- * @age: age to assign to the dog
- * @owner: owner to assign to the dog
- *
- * Return: Pointer to newly created dog
+ * @d: pointer to dog struct
  */
 
 void free_dog(dog_t *d)
