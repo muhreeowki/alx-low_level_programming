0b07e72554ce77671dfd124188697e3d8e264211
Muriuki Muchiri <muriuki@muchir.com>
2023-07-17 19:09:12 +0000

    Handling NULL pointers

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,10 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name == NULL ? "nil" : d->name);
+		printf("Age: %f\n", d->age <= 0.0 ? 0.0 : d->age);
+		printf("Owner: %s\n", d->owner == NULL ? "nil" : d->owner);
+	}
 }
