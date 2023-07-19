86ae2a0a1cda4a936b809e27c91a6e58d40af87f
Muriuki Muchiri <muriuki@muchir.com>
2023-07-19 22:28:24 +0000

    Handling division of 0

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
 		exit(99);
 	}
 
-	if ((*(argv[2]) == '/' || *(argv[2]) == '%') && (num1 == 0 || num2 == 0))
+	if ((*(argv[2]) == '/' || *(argv[2]) == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
