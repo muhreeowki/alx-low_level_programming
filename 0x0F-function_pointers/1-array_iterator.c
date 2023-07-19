fdb95b4c1593a52bbf4d5568204318d1f53e1997
Muriuki Muchiri <muriuki@muchir.com>
2023-07-19 18:51:56 +0000

    Handling NULL pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
+	if (size == 0 || action == NULL)
+		return;
+
 	for (i = 0; (size_t)i < size; i++)
 		(*action)(array[i]);
 }
