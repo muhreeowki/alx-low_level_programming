0933965eaa70727abef8a503d732d49981ce171e
Muriuki <muriuki@muchiri.com>
2023-07-27 06:20:16 -0700

    Betty documentation

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,10 @@
 #include "lists.h"
-
-void first (void)
+/**
+ * first - prints a string before main function
+ */
+void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet,
+			you must allow,\n
+			I bore my house upon my back!\n");
 }
