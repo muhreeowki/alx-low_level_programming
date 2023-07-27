09e18f1bfe0a20d159de49c4a3f2e797cdb3f43f
Muriuki <muriuki@muchiri.com>
2023-07-27 05:29:35 -0700

    Added 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ *
+ * @head: pointer to the head of the linked list
+ *
+ * Return: nothing.
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr = head;
+	list_t *next;
+
+	while (ptr->next)
+	{
+		next = ptr->next;
+		free(ptr->str);
+		free(ptr);
+		ptr = next;
+	}
+
+	free(ptr->str);
+	free(ptr);
+}
