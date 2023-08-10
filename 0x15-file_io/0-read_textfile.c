8d0765f6a317c825b3d6da64fd96f452d6981c34
Muriuki <muriuki@muchiri.com>
2023-08-10 12:46:54 +0000

    Fixing styling errors in 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,32 +2,35 @@
 #include <stdio.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it to
+ * the POSIX standard output.
  *
  * @filename: Name of the file to be opened.
- * @letters: Number of letters to read and print in the file.
+ * @letters: the number of letters it should read and print
  *
  * Return: Number of letters the program could read and print,
  * 0 is returned if ->
- * 	• file could not be opened or read,
- * 	• if filename is null,
- * 	• write function fails or does not write expected number of bytes.
+ *	• file could not be opened or read,
+ *	• if filename is null,
+ *	• write function fails or does not write expected number of bytes.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	/* 1. open the file. if unable to open return 0 */
-	/* 2. loop through the file letters times, printing a char each time using _putchar. if _putchar returns 0, return 0. */
-	/* 3. count the amount of chars printed. */
-	/* 4. return the count. */
+	/* 2. read the file's content. */
+	/* 3. write the file's content */
+	/* 4. close the file */
+	/* 5. return the count. */
 
 	int fd, count, written;
 	char *buf = malloc(sizeof(char) * letters);
 
-	if (filename == NULL || *filename == '\0' || buf == NULL)
+	if (filename == NULL || buf == NULL)
 		return (0);
 
-	if ((fd = open(filename, O_RDONLY)) == -1)
+	fd = open(filename, O_RDONLY)
+	if (fd == -1)
 		return (0);
 
 	count = read(fd, buf, letters);
@@ -35,8 +38,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	written = write(1, buf, count);
-	if (written != count || written == -1)
+	if (written != count)
 		return (0);
 
+	close(fd);
 	return (written);
 }
