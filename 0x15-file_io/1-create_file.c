87f62ba4f5ffcb4159b55b9a641fc04ba3cf46f3
Muriuki <muriuki@muchiri.com>
2023-08-10 14:01:03 +0000

    Handling NULL text_content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,15 +20,20 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT|O_WRONLY|O_TRUNC, S_IRUSR|S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
 	if (fd == -1)
 		return (-1);
 
 	length = _strlen(text_content);
-	written = length > 0 ? write(fd, text_content, length) : 0;
 
-	if (written == -1 || written != length)
-		return (-1);
+	if (length > 0)
+	{
+		written = write(fd, text_content, length);
+
+		if (written == -1 || written != length)
+			return (-1);
+	}
 
 	close(fd);
 	return (1);
