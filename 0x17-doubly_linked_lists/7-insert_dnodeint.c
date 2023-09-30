be6fc30ac56d40ad04eafa0f5ca5affb417cfa5b
Muriuki <muriuki@muchiri.com>
2023-09-30 13:37:27 +0000

    Fixed task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,12 +14,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *curr_node = NULL, *new_node = NULL;
 	unsigned int i = 0;
 
-	/* check if list is empty */
-	if (h == NULL || *h == NULL)
-		return (NULL);
-
-	/* check if idx is 0 */
-	if (idx == 0)
+	/* check if list is empty and check if idx is 0 */
+	if (h == NULL || *h == NULL || idx == 0)
 		return (add_dnodeint(h, n));
 
 	/* create new node */
@@ -31,16 +27,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	curr_node = *h;
 
 	/* loop through the list till idx - 1 */
-	for (i = 0; curr_node && i < idx - 1; i++)
-		curr_node = curr_node->next;
+	for (i = 0; i < idx - 1; i++)
+	{
+		if (curr_node->next)
+			curr_node = curr_node->next;
+		else
+			return (NULL);
+	}
 
 	/* insert the node */
 	if (curr_node && i == idx - 1)
 	{
 		new_node->prev = curr_node;
 		new_node->next = curr_node->next;
-		curr_node->next = new_node;
 		(curr_node->next)->prev = new_node;
+		curr_node->next = new_node;
 		return (new_node);
 	}
 
