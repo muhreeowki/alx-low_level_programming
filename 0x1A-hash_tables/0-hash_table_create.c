17dd833b2ca97e4c9e5b65de32b5fdd3b1730522
Muriuki <muriuki@muchiri.com>
2023-10-06 06:17:19 +0000

    Fixing task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,12 +10,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
+	hash_table_t *new_table;
+	hash_node_t **array;
 
-	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 
-	hash_node_t **array = malloc(sizeof(hash_node_t *) * size);
+	array = malloc(sizeof(hash_node_t *) * size);
 	if (array == NULL)
 		return (NULL);
 
