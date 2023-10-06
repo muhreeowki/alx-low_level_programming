ab2f7f6cc92126965a6f582d701681617112bd3c
Muriuki <muriuki@muchiri.com>
2023-10-06 06:15:40 +0000

    Fixing task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,16 +9,21 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_hash_table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
-	if (new_hash_table == NULL)
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
 		return (NULL);
 
-	new_hash_table->size = size;
-	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (new_hash_table->array == NULL)
+	hash_node_t **array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
 		return (NULL);
 
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
 
-	return (new_hash_table);
+	new_table->array = array;
+	new_table->size = size;
+
+	return (new_table);
 }
--- 0x1A-hash_tables/test.c
+++ 0x1A-hash_tables/test.c
@@ -4,19 +4,15 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code
+ * main - check the code for
  *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-    char *s;
+    hash_table_t *ht;
 
-    s = "cisfun";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    s = "Don't forget to tweet today";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    s = "98";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
+    ht = hash_table_create(1024);
+    printf("%p\n", (void *)ht);
     return (EXIT_SUCCESS);
 }
