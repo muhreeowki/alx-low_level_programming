7f3979ee9370cdc82096dcbc94a24139084a520c
Muriuki <muriuki@muchiri.com>
2023-10-07 08:02:26 +0000

    Fixing task 3

--- 0x1A-hash_tables/test.c
+++ 0x1A-hash_tables/test.c
@@ -3,6 +3,29 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
+void _hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *p;
+	char n;
+
+	for (i = 0; ht && i < ht->size; i++)
+	{
+		p = ht->array[i];
+		printf("[%lu]", i);
+		n = 0;
+		while (p)
+		{
+			if (n)
+				printf(", ");
+			printf("'%s': '%s'", p->key, p->value);
+			p = p-> next;
+			n = 1;
+		}
+		printf("\n");
+	}
+}
+
 /**
  * main - check the code for Holberton School students.
  *
@@ -11,25 +34,37 @@
 int main(void)
 {
 	hash_table_t *ht;
-	char *value;
+	int ret;
 
 	ht = hash_table_create(30);
-	hash_table_set(ht, "USA", "Washington");
-	hash_table_set(ht, "France", "Paris");
-	hash_table_set(ht, "UK", "London");
-	hash_table_set(ht, "Spain", "Madrid");
-	hash_table_set(ht, "Germany", "Berlin");
-	hash_table_set(ht, "China", "Beijing");
-	hash_table_set(ht, "Russia", "Moscow");
-	hash_table_set(ht, "Bahrain", "Manama");
-	hash_table_set(ht, "Canada", "Ottawa");
-	hash_table_set(ht, "Brazil", "Brasilia");
-	hash_table_set(ht, "Sweden", "Stockholm");
-	hash_table_set(ht, "Iceland", "Reykjavik");
-	hash_table_set(ht, "Japan", "Tokyo");
-	hash_table_set(ht, "Australia", "Canberra");
-
-	value = hash_table_get(ht, "Canada");
-	printf("%s:%s\n", "Canada", value);
+	ret = hash_table_set(ht, "USA", "Washington");
+	printf("%d\n", ret);
+	ret = hash_table_set(ht, "France", "Paris");
+	printf("%d\n", ret);
+	ret = hash_table_set(ht, "UK", "London");
+	printf("%d\n", ret);
+	ret = hash_table_set(ht, "Spain", "Madrid");
+	printf("%d\n", ret);
+	ret = hash_table_set(ht, "Germany", "Berlin");
+	printf("%d\n", ret);
+	ret = hash_table_set(ht, "China", "Beijing");
+	printf("%d\n", ret);
+	ret = hash_table_set(ht, "Russia", "Moscow");
+	printf("%d\n", ret);
+	ret = hash_table_set(ht, "Bahrain", "Manama");
+	printf("%d\n", ret);
+	ret = hash_table_set(ht, "Canada", "Ottawa");
+	printf("%d\n", ret);
+	ret = hash_table_set(ht, "Brazil", "Brasilia");
+	printf("%d\n", ret);
+	ret = hash_table_set(ht, "Sweden", "Stockholm");
+	printf("%d\n", ret);
+	ret = hash_table_set(ht, "Iceland", "Reykjavik");
+	printf("%d\n", ret);
+	ret = hash_table_set(ht, "Japan", "Tokyo");
+	printf("%d\n", ret);
+	ret = hash_table_set(ht, "Australia", "Canberra");
+	printf("%d\n", ret);
+	_hash_table_print(ht);
 	return (EXIT_SUCCESS);
 }
