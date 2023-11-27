3f86b8660f8f7187155dc2dda06f7158d9fe4c93
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-11-27 11:11:53 +0300

    Added task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,15 +4,15 @@
 
 def island_perimeter(grid):
     """Solution"""
-    perimeter = 0
+    edges = 0
+    cells = 0
 
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             if grid[y][x] == 1:
-                count = 0
-                count += 1 if y - 1 < 0 else grid[y - 1][x]
-                count += 1 if y + 1 >= len(grid) else grid[y + 1][x]
-                count += 1 if x - 1 < 0 else grid[y][x - 1]
-                count += 1 if x + 1 >= len(grid[y]) else grid[y][x + 1]
-                perimeter += 4 - count
-    return perimeter
+                cells += 1
+                if y > 0 and grid[y - 1][x] == 1:
+                    edges += 1
+                if x > 0 and grid[y][x - 1] == 1:
+                    edges += 1
+    return (cells * 4) - (edges * 2)
